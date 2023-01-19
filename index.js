144651e293751f8aa18f343ee7769a45b7e2f381
maarvins <maarvins.19@gmail.com>
2023-01-19 12:43:41 -0300

    add initial files api-nodejs

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// CONECTION DB
+const mysql2 = require("mysql2");
+const connection = mysql2.createConnection({
+  host: "containers-us-west-62.railway.app",
+  port: "6166",
+  user: "root",
+  password: "kuj3kyEJZ1IzGL7Y0gC4",
+  database: "railway",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use(express.json()); //traz o objeto no formato json
+
+app.get("/numeros", (err, res) => {
+  connection.query("SELECT * FROM numeros", function (err, data) {
+    if (err) throw err;
+    return res.status(200).json(data);
+  });
+});
+
+app.post("/numeros", (req, res) => {
+  const data = req.body.valores;
+
+  connection.query(
+    `INSERT INTO numeros VALUES (?)`,
+    [data],
+    function (err) {
+      if (err) return res.json(err);
+      return res.status(200).json("Usuário criado com sucesso!");
+    }
+  );
+});
+
+app.listen(process.env.PORT || port, function () {
+  console.log(`O servidor está rodando na porta ${port}`);
+});
